36a14be8658abdf4f19e092c06804cb5da0a5671
SebastianASU2005 <142347931+SebastianASU2005@users.noreply.github.com>
2023-08-15 16:22:44 -0300

    Add files via upload

---
+++ Tarea.py
@@ -0,0 +1,39 @@
+#Ejercicio 1
+numero1=5
+#Ejercicio 2
+numero2=3
+#Ejercicio 3
+suma=numero1+numero2
+#Ejercicio 4
+resta=numero1-numero2
+multiplicacion=numero1*numero2
+division=numero1/numero2
+#Ejercicio 5
+nombre='Sebastian'
+#Ejercicio 6
+precio=20.5
+#Ejercicio 7
+descuento=20.5*0.25
+#Ejercicio 8
+precio_final=precio-descuento
+#Ejercicio 9
+cadena='boca le gano a nacional'
+#Ejercicio 10
+longitud=len(cadena)
+#Ejercicio 11
+precio2=int(10.4)
+#Ejercicio 12
+nombre1='Sebastian'
+apellido='Astudillo'
+nombre_completo=nombre1,' ',apellido
+#Ejercicio 13
+edad=20
+edad+=5
+edad-=10
+#Ejercicio 14
+altura=1.70
+altura=altura*4
+altura=altura/3
+#Ejercicio 15
+nombreM='SEBASTIAN'.lower()
+nombreM='SEBASTIAN'.lower().title()
