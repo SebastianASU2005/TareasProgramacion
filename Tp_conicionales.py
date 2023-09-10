0174f2ab10eed05e8adfda33d3cea041bb6863ad
SebastianASU2005 <142347931+SebastianASU2005@users.noreply.github.com>
2023-09-10 14:43:38 -0300

    Update Tp_conicionales.py

--- Tp_conicionales.py
+++ Tp_conicionales.py
@@ -23,60 +23,52 @@ mes 7, se deberá imprimir ‘Comienzo de nuevo ciclo’ y solicitar al usuario 
 la cantidad de alumnos del nuevo ciclo y el arancel en $ por cada alumno, para luego
 imprimir el ingreso total en $.'''
 
-dia_dd_mm = input('dia, dd/mm')
-diaS = dia_dd_mm[0:dia_dd_mm.find(',')].lower()
-dd = dia_dd_mm[dia_dd_mm.find(' ')+1:dia_dd_mm.find('/')]
-mm = dia_dd_mm[dia_dd_mm.find('/')+1:]
+dia_dd_mm=input('dia, dd/mm')
+diaS=dia_dd_mm[0:dia_dd_mm.find(',')].lower()
+dd=dia_dd_mm[dia_dd_mm.find(' ')+1:dia_dd_mm.find('/')]
+mm=dia_dd_mm[dia_dd_mm.find('/')+1:]
 
 print(f'{diaS}/{dd}/{mm}')
 
 
-if (diaS == 'lunes' or diaS == 'martes' or diaS == 'miercoles' or diaS == 'jueves' or diaS == 'viernes' or
-   diaS == 'sabado' or diaS == 'domingo'):
-    if (int(dd) > 0 and int(dd) <= 31):
-        if (int(mm) > 0 and int(mm) <= 12):
-            print('la fecha es valida')
-        else:
-            print('fecha invalida')
-    else:
-        print('fecha invalida')
+if(diaS=='lunes' or diaS=='martes' or diaS=='miercoles' or diaS=='jueves' or diaS=='viernes' or 
+   diaS=='sabado' or diaS=='domingo' ) and (int(dd)>0 and int(dd)<=31) and (int(mm)>0 and int(mm)<=12):
+   print('la fecha es valida') 
+   if(diaS=='lunes' ):
+    print('Clases nivel inicial')
+    cantAlumnos=int(input('cuantos alumnos hay: '))
+    aprobados=int(input('cuantos alumnos aprobaron: '))    
+    print(f'el porcentaje de alumnos aprobados es: {(aprobados*100)/cantAlumnos}')
+    
+elif(diaS=='martes'):
+    print('Clases nivel intermedio')
+    cantAlumnos=int(input('cuantos alumnos hay: '))
+    aprobados=int(input('cuantos alumnos aprobaron: '))   
+    print(f'el porcentaje de alumnos aprobados es: {(aprobados*100)/cantAlumnos}')
+    
+elif(diaS=='miercoles'):
+    print('Clases nivel avanzado')
+    cantAlumnos=int(input('cuantos alumnos hay: '))
+    aprobados=int(input('cuantos alumnos aprobaron: '))
+    print(f'el porcentaje de alumnos aprobados es: {(aprobados*100)/cantAlumnos}')
 
+elif(diaS=='jueves'):
+    print('dia de practica hablada no se dicta ningun examen')
+    porc=float(input('ingrese porcentaje a clases: '))
+    if(porc>=50):
+        print('aisitio a la mayoria de las clases')
+    else:
+         print('no asistio a la mayoria de la clase')
+
+    
+elif(diaS=='viernes') and  ((int(dd)==1 and int(mm)==1) or (int(dd)==1 and int(mm)==7)) :
+
+    print('Comienzo de nuevo ciclo')
+    alum_nuevo_ciclo=int(input('ingrese cantidad de alumnos del nuevo ciclo: '))
+    cuota=float(input('ingrese valor cuota: '))
+    total=alum_nuevo_ciclo*cuota
+    print(f'el total que recibe el colegio es: {total}$')
+    
+    # print('Dia de ingles extranjero no se dictan examenes')
 else:
     print('la fecha es invalida')
-
-if (diaS == 'lunes'):
-    print('Clases nivel inicial')
-    cantAlumnos = int(input('cuantos alumnos hay'))
-    aprobados = int(input('cuantos alumnos aprobaron'))
-    print(
-        f'el porcentaje de alumnos aprobados es: {(aprobados*100)/cantAlumnos}')
-elif (diaS == 'martes'):
-    print('Clases nivel intermedio')
-    cantAlumnos = int(input('cuantos alumnos hay'))
-    aprobados = int(input('cuantos alumnos aprobaron'))
-    
-    print(
-        f'el porcentaje de alumnos aprobados es: {(aprobados*100)/cantAlumnos}')
-elif (diaS == 'miercoles'):
-    print('Clases nivel avanzado')
-    cantAlumnos = int(input('cuantos alumnos hay'))
-    aprobados = int(input('cuantos alumnos aprobaron'))
-    print(
-        f'el porcentaje de alumnos aprobados es: {(aprobados*100)/cantAlumnos}')
-elif (diaS == 'jueves'):
-    print('dia de practica hablada no se dicta ningun examen')
-    asistencia=float(input("Ingrese el porcentaje de asistencia de la clase"))
-    if (asistencia)>=50:
-        print("Asistio mas del 50% de la clase ")
-    else:
-        print("No asistio el 50% de la clase ")
-elif (diaS == 'viernes'):
-    print('Dia de ingles viajero no se dictan examenes')
-    if ((int(dd)==1 and int(mm)==1) or (int(dd)==1 and int(mm)==7)):
-        print("Comienzo del nuevo ciclo")
-        cantAlumnos=int(input("Ingresar la cantiadad de alumnos en el nuevo ciclo"))
-        precio=int(input("Ingrese el precio por persona "))
-        print(f"El precio total del nuevo ciclo es: ${precio*cantAlumnos}")
-    else:
-        print("Tenga un buen dia")
-        print(dd,mm)
